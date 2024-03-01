895d1e0cfa57f5d2619eb54e6b7253a26bf225a1
YaseenElsebaie <70989592+YaseenElsebaie@users.noreply.github.com>
2024-02-29 19:08:26 -0500

    Update test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,7 +1,19 @@
 import db
 
 from queries import FETCH_COURSE_NAME
+from queries import enroll_student
 
 
 def test_fetch_all():
     assert db.fetch_all(FETCH_COURSE_NAME) is not None
+
+def test_fetch_one():
+    assert db.fetch_one(FETCH_COURSE_NAME) is not None
+
+def test_insert():
+     assert db.insert(enroll_student)
+
+     
+test_fetch_all()
+test_fetch_one()
+test_insert()
