6db33e7d9f510681e7eb4f5464244c7b677301ca
YaseenElsebaie <70989592+YaseenElsebaie@users.noreply.github.com>
2024-03-10 23:24:55 -0400

    Update test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,17 +1,23 @@
 import db
 
 from queries import FETCH_COURSE_NAME
-from queries import enroll_student
+from queries import CREATE_DEPT2
 
 
 def test_fetch_all():
-    assert db.fetch_all(FETCH_COURSE_NAME) is not None
+
+    assert db.fetch_all("SELECT * FROM student") is not None
+
+    assert db.fetch_all("SELECT * FROM test") is None
 
 def test_fetch_one():
-    assert db.fetch_one(FETCH_COURSE_NAME) is not None
+
+    assert db.fetch_one("SELECT * FROM student") is not None
+
+    assert db.fetch_one("SELECT * FROM test") is None
 
 def test_insert():
-     assert db.insert(enroll_student)
+     assert db.insert(CREATE_DEPT2)
 
      
 test_fetch_all()
