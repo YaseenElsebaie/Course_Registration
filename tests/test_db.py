efd6161d080d562c663662f056872c0dafdcabf9
YaseenElsebaie <70989592+YaseenElsebaie@users.noreply.github.com>
2024-04-01 17:20:18 -0400

    Update test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,18 +7,24 @@ def test_fetch_all():
 
     assert db.fetch_all("SELECT * FROM student") is not None
 
-    assert db.fetch_all("SELECT * FROM test") is None
+def test_fetch_all_empty():
+    
+    assert db.fetch_all("SELECT * FROM choice") == () 
 
 def test_fetch_one():
 
     assert db.fetch_one("SELECT * FROM student") is not None
 
-    assert db.fetch_one("SELECT * FROM test") is None
+def test_fetch_one_empty():
+
+    assert db.fetch_one("SELECT * FROM choice") is None 
 
 def test_insert():
-     assert db.insert(TEST_CREATE_DEPT2)
+    assert db.insert(TEST_CREATE_DEPT2) is None
 
      
 test_fetch_all()
+test_fetch_all_empty()
 test_fetch_one()
+test_fetch_one_empty()
 test_insert()
