45d68afc03f8fcc1a9f4cfdb4b1e969fe589bc5b
YaseenElsebaie <70989592+YaseenElsebaie@users.noreply.github.com>
2024-04-09 17:24:14 -0400

    Update test_db.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,30 +1,38 @@
 import db
 
-from queries import TEST_CREATE_DEPT2
-
+TEST_STUDENT_ID = "Y"
+STUDENT_ID_FLD = "Student_ID" 
 
 def test_fetch_all():
+    result = db.fetch_all("SELECT * FROM student")
+    assert isinstance(result, list)
+    assert len(result) > 0
 
-    assert db.fetch_all("SELECT * FROM student") is not None
 
 def test_fetch_all_empty():
-    
-    assert db.fetch_all("SELECT * FROM choice") == () 
+    result = db.fetch_all("SELECT * FROM choice")
+    assert result == () 
+
 
 def test_fetch_one():
+    query = f"SELECT * FROM student where Student_ID = '{TEST_STUDENT_ID}'"
+    result = db.fetch_one(query)
+    assert isinstance(result, dict)
+    assert result[STUDENT_ID_FLD] == TEST_STUDENT_ID
 
-    assert db.fetch_one("SELECT * FROM student") is not None
 
 def test_fetch_one_empty():
+    result = db.fetch_one("SELECT * FROM choice")
+    assert result is None 
 
-    assert db.fetch_one("SELECT * FROM choice") is None 
 
 def test_insert():
-    assert db.insert(TEST_CREATE_DEPT2) is None
+    result = db.insert("INSERT INTO Department (Department_Name, Department_Address, Department_Email) VALUES ('D8', 'D8', 'D8')")
+    assert result is None
+
+
+def test_delete():
+    result = db.insert("DELETE FROM Department WHERE Department_Name = 'D8' AND Department_Address = 'D8' AND Department_Email = 'D8' ")
+    assert result is None
 
      
-test_fetch_all()
-test_fetch_all_empty()
-test_fetch_one()
-test_fetch_one_empty()
-test_insert()
